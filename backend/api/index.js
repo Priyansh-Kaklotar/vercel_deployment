90c6e0dea857ca36883b439a9f208af8d59e3886
Priyansh-Kaklotar <priyanshhh8@gmail.com>
2025-08-07 19:04:50 +0530

    upadte main 4

--- backend/api/index.js
+++ backend/api/index.js
@@ -9,13 +9,20 @@ import sendMail from "../utils/mailSender.js";
 import User from "../models/user-model.js";
 
 const app = express();
+// app.use(
+//   cors({
+//     origin: "https://vercel-deployment-client-seven.vercel.app/login", // replace with your actual frontend Vercel URL
+//     methods: ["GET", "POST", "PUT", "DELETE"],
+//     credentials: true, // if you're using cookies or sessions
+//   })
+// );
+
 app.use(
   cors({
-    origin: "https://vercel-deployment-client-seven.vercel.app/login", // replace with your actual frontend Vercel URL
-    methods: ["GET", "POST", "PUT", "DELETE"],
-    credentials: true, // if you're using cookies or sessions
+    origin: "*",
   })
 );
+
 //must for every backend file
 dotenv.config();
 app.use(express.json());
