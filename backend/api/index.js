ad1547613cabceeb2f9ec94635d7fe1c19d76e5f
Priyansh-Kaklotar <priyanshhh8@gmail.com>
2025-08-09 16:20:28 +0530

    priyansh 21

--- backend/api/index.js
+++ backend/api/index.js
@@ -15,16 +15,10 @@ app.use((req, res, next) => {
   if (allowedOrigins.includes(origin)) {
     res.setHeader("Access-Control-Allow-Origin", origin);
   }
-  res.setHeader(
-    "Access-Control-Allow-Methods",
-    "GET, POST, PUT, DELETE, OPTIONS"
-  );
+  res.setHeader("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE");
   res.setHeader("Access-Control-Allow-Headers", "Content-Type, Authorization");
   res.setHeader("Access-Control-Allow-Credentials", "true");
 
-  if (req.method === "OPTIONS") {
-    return res.status(200).end();
-  }
   next();
 });
 
